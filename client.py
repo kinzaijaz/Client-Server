8303af4ab9aee85d5ae302ea22f2b47e9b730688
kinzaijaz <bsef14m506@pucit.edu.pk>
2016-05-18 21:36:04 +0500

    Update client.py

--- client.py
+++ client.py
@@ -5,12 +5,18 @@ from Tools.Scripts.treesync import raw_input
 s = socket.socket()
 host = socket.gethostname()
 port = 5182  # Reserve a port for your service.
+def receive(d):
+    while True:
+        data1 = (s.recv(1024)).decode()
+        print(str(data1))
+        if not data1:
+            break
 
 s.connect((host, port))
-print(s.recv(1024))
+start_new_thread(receive, (None,))
 while True:
     data = raw_input()
     s.send(data)
-    print(s.recv(1024))
+    start_new_thread(receive, (None,))
 
 s.close  # Close the socket when done
