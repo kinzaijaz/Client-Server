0b768ec97ea7b19cc8d03c7f15255ae182d12d63
kinzaijaz <bsef14m506@pucit.edu.pk>
2016-05-18 21:42:13 +0500

    Update server.py

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from _thread import *
 
 HOST = ''  # Symbolic name meaning all available interfaces
 PORT = 5182  # Arbitrary non-privileged port
+global conn
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print ('Socket created')
@@ -25,38 +26,42 @@ print ('Socket bind complete')
 s.listen(10)
 print('Socket now listening')
 
-
 # Function for handling connections. This will be used to create threads
-def clientthread(conn):
+def clientthread(i):
     # Sending message to connected client
-    conn.send('Welcome to the server. Type something and hit enter\n')  # send only takes string
-
+    if i%2 ==0:
+        conn[i].send(b'Welcome to the server. Type something and hit enter\n')  # send only takes string
+    arr = ['Client1', 'Client2']
     # infinite loop so that function do not terminate and thread do not end.
     while True:
-
         # Receiving from client
-        data = conn.recv(1024)
-        reply = 'OK...' + data
+        data = (conn[i].recv(1024)).decode()
         if not data:
             break
-
-        conn.sendall(reply)
+        print ('Receiving from ' + str(arr[i]) + ': '+ str(data))
+        if not i % 2 == 0:
+            print('Sending to ' + str(arr[i - 1]))
+            data = str(arr[i]) + ': ' + str(data)
+            data = data.encode()
+            conn[i-1].sendall(data)
+        else:
+            print('Sending to ' + str(arr[i + 1]))
+            data = str(arr[i]) + ': ' + str(data)
+            data = data.encode()
+            conn[i + 1].sendall(data)
 
     # came out of loop
-    conn.close()
+    conn[i].close()
 
 
-# now keep talking with the client
-def start_new_thread(clientthread, param):
-    pass
-
-
-while 1:
+conn= [0, 0]
+for i in range (0,2):
     # wait to accept a connection - blocking call
-    conn, addr = s.accept()
+    conn[i], addr = s.accept()
     print('Connected with ' + addr[0] + ':' + str(addr[1]))
 
     # start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread, (conn,))
+    start_new_thread(clientthread, (i,))
 
 s.close()
+
