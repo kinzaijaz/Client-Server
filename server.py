1ae96219a18dbdd501a188b7c54bd2477087ec42
kinzaijaz <bsef14m506@pucit.edu.pk>
2016-05-26 23:55:50 +0500

    module 2

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import sys
 from _thread import *
 
 HOST = ''  # Symbolic name meaning all available interfaces
-PORT = 5182  # Arbitrary non-privileged port
+PORT = 5162  # Arbitrary non-privileged port
 global conn
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,42 +23,49 @@ except socket.error as msg:
 print ('Socket bind complete')
 
 # Start listening on socket
-s.listen(2)
+s.listen(20)
 print('Socket now listening')
-
 # Function for handling connections. This will be used to create threads
 def clientthread(i):
-    # Sending message to connected client
-    conn[i].send(b'Welcome to the server. Type something and hit enter\n')  # send only takes string
-    arr = ['Client1', 'Client2']
     # infinite loop so that function do not terminate and thread do not end.
     while True:
         # Receiving from client
-        data = (conn[i].recv(1024)).decode()
+        data = (conn.recv(1024)).decode()
         if not data:
             break
-        print ('Receiving from ' + str(arr[i]) + ': '+ str(data))
-        if not i % 2 == 0:
-            print('Sending to ' + str(arr[i - 1]))
-            data = str(arr[i]) + ': ' + str(data)
+        print ('Receiving from ' + str(client[i]) + ': '+ str(data))
+        if conn==arr[0]['conn']:
+            print('Sending to ' + str(client[i]))
+            data = str(client[i]) + ': ' + str(data)
             data = data.encode()
-            conn[i-1].sendall(data)
-        else:
-            print('Sending to ' + str(arr[i + 1]))
-            data = str(arr[i]) + ': ' + str(data)
+            conn.sendall(data)
+        elif conn==arr[1]['conn']:
+            print('Sending to ' + str(client[i]))
+            data = str(client[i]) + ': ' + str(data)
             data = data.encode()
-            conn[i + 1].sendall(data)
-
+            conn.sendall(data)
     # came out of loop
     conn[i].close()
 
+def onlineusers(arr):
+    for i in range(0, len(arr)):
+        d = str(arr[i]['name'] + "is online")
+        d = d.encode()
+        arr[i]['conn'].send(d)
 
-conn= [0, 0]
-for i in range (0,3):
+client=['Client1','Client2','Client3','Client4','Client5','Client6','Client7','Client8','Client9','Client10','Client11','Client12','Client13','Client14','Client15','Client16','Client17','Client18','Client19','Client20']
+dict={}
+arr=[]
+
+for i in range (30):
     # wait to accept a connection - blocking call
-    conn[i], addr = s.accept()
-    print('Connected with ' + addr[0] + ':' + str(addr[1]))
-
+    conn, addr = s.accept()
+    dict['conn']=conn
+    dict['name']=client[i]
+    dict['address']=addr[1]
+    arr.append(dict)
+    print('Connected with ' + addr[0] + ':' + str(addr[1])+ ':' + client[i])
+    onlineusers(arr)
     # start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
     start_new_thread(clientthread, (i,))
 
